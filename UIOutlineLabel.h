552c749dca463111e892d1358f15f629f054ef62
Gladiola_ola_ola <gladiola.olaola@ukr.net>
2024-07-21 18:54:17 +0300

    Initial commit

---
+++ UIOutlineLabel.h
@@ -0,0 +1,13 @@
+//
+//  UIOutlineLabel.h
+//  Created by Shogo Ishida on 2014/04/27.
+//
+
+@import UIKit;
+
+@interface UIOutlineLabel : UILabel
+
+@property(nonatomic) UIColor *outlineColor;
+@property(nonatomic) NSInteger outlineSize;
+
+@end
